4526433585b257d0d8e2e525e8ec0e44ee932588
MikesVibe <miki889-99@wp.pl>
2023-01-08 23:16:54 +0100

    Implement Autofac for dependecy injection

--- DbConfigurator/App.xaml.cs
+++ DbConfigurator/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using DbConfigurator.UI;
+using DbConfigurator.UI.Startup;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,13 @@ namespace DbConfigurator
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
---
+++ DbConfigurator/Startup/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using DbConfigurator.UI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConfigurator.UI.Startup
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+
+
+            return builder.Build();
+        }
+    }
+}
