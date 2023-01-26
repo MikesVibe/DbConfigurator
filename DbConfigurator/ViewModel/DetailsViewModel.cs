f56177ce24b18f7ec7c5dafc66d819cba7d72a06
MikesVibe <miki889-99@wp.pl>
2023-01-26 17:18:05 +0100

    Positioned some views

--- DbConfigurator/ViewModel/DetailsViewModel.cs
+++ DbConfigurator/ViewModel/DetailsViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using DbConfigurator.Model;
 using DbConfigurator.UI.ViewModel.Interfaces;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace DbConfigurator.UI.ViewModel
 {
@@ -26,13 +29,40 @@ namespace DbConfigurator.UI.ViewModel
                     Email = "John.Doe@comp.net"
                 });
             }
+
+            GridDataCollection = Recipients_ObservableCollection;
+        }
+
+
+
+        private void GridDataCollectionOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            GridDataCollectionChangedCommand.Execute(e);
+        }
+        private void ExecuteGridDataCollectionChangedCommand(NotifyCollectionChangedEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
         }
 
 
         public int DefaultRowIndex { get { return 0; } }
         public ObservableCollection<Recipient> Recipients_ObservableCollection { get; }
-
+        public ObservableCollection<Recipient> GridDataCollection
+        {
+            get => _gridDataCollection;
+            set
+            {
+                _gridDataCollection = value;
+                _gridDataCollection.CollectionChanged += GridDataCollectionOnCollectionChanged;
+            }
+        }
         public Recipient SelectedRecipient { get; set; }
+        public DelegateCommand<NotifyCollectionChangedEventArgs> GridDataCollectionChangedCommand =>
+             _gridDataCollectionChangedCommand ?? (_gridDataCollectionChangedCommand = new DelegateCommand<NotifyCollectionChangedEventArgs>(ExecuteGridDataCollectionChangedCommand));
+
+
+        public DelegateCommand<NotifyCollectionChangedEventArgs> _gridDataCollectionChangedCommand;
+        private ObservableCollection<Recipient> _gridDataCollection;
 
 
     }
