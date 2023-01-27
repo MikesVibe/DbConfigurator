52b5f9b7879bc0bc8416261a2f3df425f35aec5e
MikesVibe <miki889-99@wp.pl>
2023-01-27 12:32:48 +0100

    Added binding between DataGrid and Command

---
+++ DbConfigurator/Commands/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DbConfigurator.UI.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new
+            ArgumentNullException("execute"); _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter) { _execute(parameter); }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+    }
+}
--- DbConfigurator/ViewModel/DetailsViewModel.cs
+++ DbConfigurator/ViewModel/DetailsViewModel.cs
@@ -5,10 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DbConfigurator.UI.ViewModel
 {
@@ -16,9 +19,16 @@ namespace DbConfigurator.UI.ViewModel
     {
         public DetailsViewModel()
         {
+            InitRecipients();
+
+
+            CellEditEndingCommand = new RelayCommand(CellEditEndingCommandExecute);
+
+        }
+
+        private void InitRecipients()
+        {
             Recipients_ObservableCollection = new ObservableCollection<Recipient>();
-
-
 
             for (int i = 0; i < 5; i++)
             {
@@ -29,41 +39,40 @@ namespace DbConfigurator.UI.ViewModel
                     Email = "John.Doe@comp.net"
                 });
             }
-
-            GridDataCollection = Recipients_ObservableCollection;
         }
 
-
-
-        private void GridDataCollectionOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            GridDataCollectionChangedCommand.Execute(e);
-        }
-        private void ExecuteGridDataCollectionChangedCommand(NotifyCollectionChangedEventArgs e)
-        {
-            MessageBox.Show(e.ToString());
-        }
-
+        public RelayCommand CellEditEndingCommand { get; set; }
 
         public int DefaultRowIndex { get { return 0; } }
-        public ObservableCollection<Recipient> Recipients_ObservableCollection { get; }
-        public ObservableCollection<Recipient> GridDataCollection
+        public ObservableCollection<Recipient> Recipients_ObservableCollection { get; set; }
+
+        public Recipient SelectedRecipient { get; set; }
+
+        private void CellEditEndingCommandExecute(object obj)
         {
-            get => _gridDataCollection;
-            set
+            //var recipient = (Recipient)obj;
+            //var Id = Recipients_ObservableCollection.Where(rec => recipient.Id == rec.Id).FirstOrDefault()?.Id;
+            //if(Id == null)
+            //    return;
+
+            //Recipients_ObservableCollection[(int)Id] = recipient;
+            //var a = 0;
+
+            var selectedCells = (IEnumerable<DataGridCellInfo>)obj;
+            object item;
+            DataGridColumn column;
+            FrameworkElement value;
+
+            foreach (var cell in selectedCells)
             {
-                _gridDataCollection = value;
-                _gridDataCollection.CollectionChanged += GridDataCollectionOnCollectionChanged;
+                item = cell.Item;
+                column = cell.Column;
+                value = column.GetCellContent(item);
+
+
             }
         }
-        public Recipient SelectedRecipient { get; set; }
-        public DelegateCommand<NotifyCollectionChangedEventArgs> GridDataCollectionChangedCommand =>
-             _gridDataCollectionChangedCommand ?? (_gridDataCollectionChangedCommand = new DelegateCommand<NotifyCollectionChangedEventArgs>(ExecuteGridDataCollectionChangedCommand));
 
-
-        public DelegateCommand<NotifyCollectionChangedEventArgs> _gridDataCollectionChangedCommand;
         private ObservableCollection<Recipient> _gridDataCollection;
-
-
     }
 }
