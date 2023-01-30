6395a21449e018714d36afb96282a0dbb28efb62
MikesVibe <miki889-99@wp.pl>
2023-01-30 10:11:16 +0100

    Added ModelWrapper and NotifyDataErrorInfoBase classes

---
+++ DbConfigurator/Wrapper/ModelWrapper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConfigurator.UI.Wrapper
+{
+    public class ModelWrapper<T> : NotifyDataErrorInfoBase
+    {
+        public ModelWrapper(T model) 
+        {
+            Model = model;
+        }
+
+        public T Model { get; }
+
+        protected virtual TValue GetValue<TValue>([CallerMemberName] string propertyName = null)
+        {
+            return (TValue)typeof(T).GetProperty(propertyName).GetValue(Model);
+        }
+    
+        protected virtual void SetValue<TValue>(TValue value, [CallerMemberName] string propertyName = null)
+        {
+            typeof(T).GetProperty(propertyName).SetValue(Model, value);
+            OnPropertyChanged(propertyName);
+            ValidatePropertyInternal(propertyName, value);
+        }
+
+        private void ValidatePropertyInternal<TValue>(string propertyName, TValue? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
