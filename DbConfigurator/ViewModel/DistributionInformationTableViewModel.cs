f3b9fa9d90e6412986f5174324f66a66f68745de
MikesVibe <miki889-99@wp.pl>
2023-02-07 14:59:47 +0100

    Binded SelectionChanged Event to Command

--- DbConfigurator/ViewModel/DistributionInformationTableViewModel.cs
+++ DbConfigurator/ViewModel/DistributionInformationTableViewModel.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DbConfigurator.UI.ViewModel
 {
@@ -32,7 +33,7 @@ namespace DbConfigurator.UI.ViewModel
 
             DisInfoLookup_ObservableCollection = new ObservableCollection<DistributionInformation>();
 
-
+            SelectionChangedCommand = new DelegateCommand(OnSelectionChanged);
         }
 
         public override async Task LoadAsync()
@@ -74,40 +75,12 @@ namespace DbConfigurator.UI.ViewModel
 
         }
 
-        private void DistributionInformation_ObservableCollection_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (!HasChanges)
-            {
-                HasChanges = _distributionInformationRepository.HasChanges();
-            }
-            else
-            {
 
-
-            }
-            if (e.PropertyName == nameof(DistributionInformationWrapper.HasErrors))
-            {
-                ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
-            }
-        }
 
         protected override void OnDeleteExecute()
         {
             throw new NotImplementedException();
         }
-        //protected override bool OnSaveCanExecute()
-        //{
-        //    return SelectedDisInfoLookup != null
-        //        && !SelectedDisInfoLookup.HasErrors
-        //        && HasChanges;
-        //}
-        //protected override void OnSaveExecute()
-        //{
-        //    _distributionInformationRepository.SaveAsync();
-        //    HasChanges = _distributionInformationRepository.HasChanges();
-        //    Id = SelectedDisInfoLookup.Id;
-
-        //}
         protected override bool OnSaveCanExecute()
         {
             return true;
@@ -119,96 +92,98 @@ namespace DbConfigurator.UI.ViewModel
             Id = SelectedDistributionInformation.Id;
 
         }
+        private void DistributionInformation_ObservableCollection_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (!HasChanges)
+            {
+                HasChanges = _distributionInformationRepository.HasChanges();
+            }
+            if (e.PropertyName == nameof(DistributionInformationWrapper.HasErrors))
+            {
+                ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+            }
+        }
+        private void OnSelectionChanged()
+        {
+            if (SelectedDistributionInformation != null)
+            {
+                SelectedAreaIndex = Area_Collection.Where(a => a.Id == _selectedDistributionInformation.Country.BuisnessUnit.Area.Id).First().Id - 1;
+
+                SelectedBuisnessUnitIndex = BuisnessUnit_Collection.Where(b => b.Id == _selectedDistributionInformation.Country.BuisnessUnit.Id).First().Id - 1;
+                SelectedcCountryIndex = Country_Collection.Where(c => c.Id == _selectedDistributionInformation.Country.Id).First().Id - 1;
+                //SelectedcPriorityIndex = Priority_Collection.Where(p => p.Id == _selectedDistributionInformation.Priority.Id).First().Id - 1;
+
+                //OnPropertyChanged(nameof(SelectedAreaIndex));
+                //OnPropertyChanged(nameof(SelectedBuisnessUnitIndex));
+                //OnPropertyChanged(nameof(SelectedcCountryIndex));
+                //OnPropertyChanged(nameof(SelectedcPriorityIndex));
+            }
+        }
 
 
         public int DefaultRowIndex { get { return 0; } }
+        public int SelectedAreaIndex
+        {
+            get { return _selectedAreaIndex; }
+            set 
+            {
+                _selectedAreaIndex = value;
+                OnPropertyChanged();
+            }
+        }
+        public int SelectedBuisnessUnitIndex
+        {
+            get { return _selectedBuisnessUnitIndex; }
+            set
+            {
+                _selectedBuisnessUnitIndex = value;
+                OnPropertyChanged();
+            }
+        }
+        public int SelectedcCountryIndex
+        {
+            get { return _selectedcCountryIndex; }
+            set
+            {
+                _selectedcCountryIndex = value;
+                OnPropertyChanged();
+            }
+        }
+        public int SelectedcPriorityIndex
+        {
+            get { return _selectedcPriorityIndex; }
+            set
+            {
+                _selectedcPriorityIndex = value;
+                OnPropertyChanged();
+            }
+        }
         public DistributionInformation SelectedDistributionInformation
         {
-            get 
-            {
-                if (_selectedDistributionInformation != null)
-                {
-                    SelectedAreaIndex = Area_Collection.Where(a => a.Id == _selectedDistributionInformation.Country.BuisnessUnit.Area.Id).First().Id - 1;
-                    SelectedBuisnessUnitIndex = BuisnessUnit_Collection.Where(b => b.Id == _selectedDistributionInformation.Country.BuisnessUnit.Id).First().Id - 1;
-                    SelectedcCountryIndex = Country_Collection.Where(c => c.Id == _selectedDistributionInformation.Country.Id).First().Id - 1;
-                    //SelectedcPriorityIndex = Priority_Collection.Where(p => p.Id == _selectedDistributionInformation.Priority.Id).First().Id - 1;
-
-                }
-
-
-                OnPropertyChanged(nameof(SelectedAreaIndex));
-                OnPropertyChanged(nameof(SelectedBuisnessUnitIndex));
-                OnPropertyChanged(nameof(SelectedcCountryIndex));
-                //OnPropertyChanged(nameof(SelectedcPriorityIndex));
-
-                return _selectedDistributionInformation; 
-            }
+            get { return _selectedDistributionInformation; }
             set
             {
                 _selectedDistributionInformation = value;
                 OnPropertyChanged();
             }
         }
-        private int _selectedAreaIndex;
-        public int SelectedAreaIndex
-        {
-            get { return _selectedAreaIndex; }
-            set { _selectedAreaIndex = value; }
-        }
-        private int _selectedBuisnessUnitIndex;
-        public int SelectedBuisnessUnitIndex
-        {
-            get { return _selectedBuisnessUnitIndex; }
-            set { _selectedBuisnessUnitIndex = value; }
-        }
-        private int _selectedcCountryIndex;
-
-        public int SelectedcCountryIndex
-        {
-            get { return _selectedcCountryIndex; }
-            set { _selectedcCountryIndex = value; }
-        }
-        private int _selectedcPriorityIndex;
-
-        public int SelectedcPriorityIndex
-        {
-            get { return _selectedcPriorityIndex; }
-            set { _selectedcPriorityIndex = value; }
-        }
-
-
-        //private Area _selectedAreaItem;
-
-        //public Area SelectedAreaItem
-        //{
-        //    get { return _selectedAreaItem; }
-        //    set { _selectedAreaItem = value; }
-        //}
-
-
-        //public ObservableCollection<DistributionInformationWrapper> DistributionInformation_ObservableCollection { get; set; }
-
-        //public DistributionInfoLookupWrapper SelectedDisInfoLookup
-        //{
-        //    get { return _selectedDisInfoLookup; }
-        //    set
-        //    {
-        //        _selectedDisInfoLookup = value;
-        //        OnPropertyChanged();
-        //    }
-        //}
-
         public ObservableCollection<DistributionInformation> DisInfoLookup_ObservableCollection { get; set; }
         public ObservableCollection<Area> Area_Collection { get; set; }
         public ObservableCollection<BuisnessUnit> BuisnessUnit_Collection { get; set; }
         public ObservableCollection<Country> Country_Collection { get; set; }
         public ObservableCollection<Priority> Priority_Collection { get; private set; }
+        public ICommand SelectionChangedCommand { get; set; }
+
+
 
         private IDistributionInformationRepository _distributionInformationRepository;
         private ICountryRepository _countryRepository;
         private IRecipientRepository _recipientRepository;
         private IEventAggregator _eventAggregator;
         private DistributionInformation _selectedDistributionInformation;
-        //private DistributionInfoLookupWrapper _selectedDisInfoLookup;
+        private int _selectedAreaIndex;
+        private int _selectedBuisnessUnitIndex;
+        private int _selectedcCountryIndex;
+        private int _selectedcPriorityIndex;
     }
 }
