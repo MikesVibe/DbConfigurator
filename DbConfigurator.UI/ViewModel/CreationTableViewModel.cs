0851994c5a695c2ce8f4b7ae4e883f3a4c4a834f
MikesVibe <miki889-99@wp.pl>
2023-05-31 14:34:01 +0200

    Add: Window for Country creation and BuissnesUnit creation

--- DbConfigurator.UI/ViewModel/CreationTableViewModel.cs
+++ DbConfigurator.UI/ViewModel/CreationTableViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using DbConfigurator.Model;
 using DbConfigurator.Model.DTOs;
 using DbConfigurator.UI.Startup;
+using DbConfigurator.UI.ViewModel.Interfaces;
 using DbConfigurator.UI.Windows;
+using Prism.Commands;
 using Prism.Events;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DbConfigurator.UI.ViewModel
 {
@@ -16,6 +19,9 @@ namespace DbConfigurator.UI.ViewModel
     {
         private readonly IDataModel _dataModel;
         private readonly AutoMapperConfig _autoMapper;
+
+        public ICommand AddCountryCommand { get; set; }
+        public ICommand AddBuisnessUnitCommand { get; set; }
 
         public ObservableCollection<CountryDto> Countries { get; set; } = new ObservableCollection<CountryDto>();
 
@@ -26,6 +32,9 @@ namespace DbConfigurator.UI.ViewModel
         {
             _dataModel = dataModel;
             _autoMapper = autoMapper;
+
+            AddBuisnessUnitCommand = new DelegateCommand(OnAddBuisnessUnitExecute);
+            AddCountryCommand = new DelegateCommand(OnAddCountryExecute);
         }
 
         public override async Task LoadAsync()
@@ -63,6 +72,52 @@ namespace DbConfigurator.UI.ViewModel
             }
         }
 
+        private void OnAddBuisnessUnitExecute()
+        {
+            AddBuisnessUnitViewModel viewModel = new AddBuisnessUnitViewModel();
+            AddBuisnessUnitWindow addAreaWindow = new AddBuisnessUnitWindow();
+            addAreaWindow.DataContext = viewModel;
+            viewModel.Window = addAreaWindow;
+
+            bool? result = addAreaWindow.ShowDialog();
+
+            if (result == true)
+            {
+                // User clicked the Add button
+                // Perform any actions with the entered area name here
+                string areaName = viewModel.AreaName;
+                // ...
+            }
+            else
+            {
+                // User clicked the Cancel button or closed the window
+                // Handle cancellation logic here
+            }
+        }
+
+        private void OnAddCountryExecute()
+        {
+            AddCountryViewModel viewModel = new AddCountryViewModel();
+            AddCountryWindow addAreaWindow = new AddCountryWindow();
+            addAreaWindow.DataContext = viewModel;
+            viewModel.Window = addAreaWindow;
+
+            bool? result = addAreaWindow.ShowDialog();
+
+            if (result == true)
+            {
+                // User clicked the Add button
+                // Perform any actions with the entered area name here
+                string areaName = viewModel.AreaName;
+                // ...
+            }
+            else
+            {
+                // User clicked the Cancel button or closed the window
+                // Handle cancellation logic here
+            }
+        }
+
         protected override bool OnRemoveCanExecute()
         {
             return false;
@@ -72,5 +127,6 @@ namespace DbConfigurator.UI.ViewModel
         {
             throw new NotImplementedException();
         }
+
     }
 }
---
+++ DbConfigurator.UI/ViewModel/Interfaces/AddCountryViewModel.cs
@@ -0,0 +1,76 @@
+﻿using DbConfigurator.UI.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DbConfigurator.UI.ViewModel.Interfaces
+{
+    class AddCountryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _areaName;
+        public string AreaName
+        {
+            get { return _areaName; }
+            set
+            {
+                _areaName = value;
+                OnPropertyChanged(nameof(AreaName));
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public AddCountryViewModel()
+        {
+            //AddCommand = new RelayCommand(Add, CanAdd);
+            //CancelCommand = new RelayCommand(Cancel);
+        }
+
+        private void Add(object parameter)
+        {
+            // Add button click logic here
+            // Perform any desired operations with the entered area name
+            // For example, you could close the window and return the value to the main window
+            CloseWindow(true);
+        }
+
+        private bool CanAdd(object parameter)
+        {
+            // Enable or disable the Add button based on any conditions
+            // For example, you can check if the area name is not empty
+            return !string.IsNullOrEmpty(AreaName);
+        }
+
+        private void Cancel(object parameter)
+        {
+            // Cancel button click logic here
+            // For example, you could simply close the window without performing any operations
+            CloseWindow(false);
+        }
+
+        private void CloseWindow(bool dialogResult)
+        {
+            // Close the window and set the dialog result
+            if (Window != null)
+            {
+                Window.DialogResult = dialogResult;
+                Window.Close();
+            }
+        }
+
+        // Reference to the window
+        public AddCountryWindow Window { get; set; }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
